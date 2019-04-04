4e7441c9a5d11716e7e761f0fcc0fb370435c724
Masato Hori <rillomas@gmail.com>
2019-04-04 16:43:45 +0900

    connected to Halide function

--- include/back_projection.hpp
+++ include/back_projection.hpp
@@ -7,7 +7,17 @@ enum Result {
 	ERROR,
 };
 
-struct Image {
+class Image {
+public:
+	Image(
+		uint8_t* _data,
+		int _width,
+		int _height,
+		int _channels)
+		: data(_data)
+		, width(_width)
+		, height(_height)
+		, channels(_channels){}
 	uint8_t* data;
 	int width;
 	int height;
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <chrono>
 #include "back_projection.hpp"
 
 TEST(IOTest, CanReadAndWrite) {
@@ -8,6 +9,14 @@ TEST(IOTest, CanReadAndWrite) {
 	cv::Mat input = cv::imread(in_path);
 	ASSERT_FALSE(input.empty());
 	cv::Mat output(input.size(), input.type());
-
-	cv::imwrite("lena_out.png", input);
+	ct::Image ctin(input.data, input.cols, input.rows, input.channels());
+	ct::Image ctout(output.data, output.cols, output.rows, output.channels());
+	auto start = std::chrono::high_resolution_clock::now();
+	auto ret = ct::calc_back_projection(&ctout, ctin);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+	std::cout
+		<< msec << " msec" << std::endl;
+	EXPECT_EQ(ct::Result::OK, ret);
+	cv::imwrite("lena_out.png", output);
 }
