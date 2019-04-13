7e7d6e0f46f7605a0f5ea77478cc4fc26319766f
Masato Hori <rillomas@gmail.com>
2019-04-13 16:55:33 +0900

    added a basic generator and test for Halide AOT

--- back_projection.cpp
+++ back_projection.cpp
@@ -1,53 +1,5 @@
 #include "Halide.h"
-#include "back_projection.hpp"
 using namespace Halide;
-
-namespace ct {
-
-	
-// static Func calc_back_projection_generic(
-// 	Buffer<uint8_t> inbuf,
-// 	Var x,
-// 	Var y,
-// 	Var c) {
-// 	// cast raw buffer to Halide buffer
-// 	auto val = cast<float>(inbuf(x,y,c));
-// 	val = val * 1.5f;
-// 	auto outval = cast<uint8_t>(clamp(val, 0.0f, 255.0f));
-// 	Func backproj;
-// 	backproj(x,y,c) = outval;
-// 	return backproj;
-// }
-
-// Result calc_back_projection_generic(
-// 	Image* output,
-// 	const Image& input) {
-// 	Var x,y,c;
-// 	Buffer<uint8_t> inbuf(input.data, input.width, input.height, input.channels);
-// 	auto backproj = calc_back_projection_generic(inbuf, x, y, c);
-// 	Buffer<uint8_t> outbuf(output->data, output->width, output->height, output->channels);
-// 	backproj.realize(outbuf);
-// 	return OK;
-// }
-
-// Result calc_back_projection_cpu(
-// 	Image* output,
-// 	const Image& input) {
-// 	Var x,y,c;
-// 	Buffer<uint8_t> inbuf(input.data, input.width, input.height, input.channels);
-// 	auto backproj = calc_back_projection_generic(inbuf, x, y, c);
-// 	Buffer<uint8_t> outbuf(output->data, output->width, output->height, output->channels);
-// 	backproj.vectorize(x, 8).parallel(y);
-// 	backproj.realize(outbuf);
-// 	return OK;
-// }
-
-} // namespace ct
-
-enum Backend {
-	GENERIC,
-	CPU,
-};
 
 class BackProjectionGenerator : public Generator<BackProjectionGenerator> {
 public:
@@ -68,9 +20,13 @@ public:
 		}
 	}
 
-	Input<Buffer<uint8_t>> input{"input",2};
-	Output<Buffer<uint8_t>> output{"brighter",2};
+	Input<Buffer<uint8_t>> input{"input",3};
+	Output<Buffer<uint8_t>> output{"brighter",3};
 	Var x,y,c;
+	enum Backend {
+		GENERIC,
+		CPU,
+	};
 	GeneratorParam<Backend> backend {
 		"backend",
 		GENERIC,
--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <chrono>
-#include "back_projection.hpp"
+#include "HalideBuffer.h"
+#include "back_projection_basic.h"
 
 TEST(IOTest, CanReadAndWrite) {
 	const std::string in_path("../test/data/lena.png");
@@ -10,23 +11,14 @@ TEST(IOTest, CanReadAndWrite) {
 	ASSERT_FALSE(input.empty());
 	cv::Mat generic_output(input.size(), input.type());
 	cv::Mat cpu_output(input.size(), input.type());
-	ct::Image ctin(input.data, input.cols, input.rows, input.channels());
-	ct::Image generic_out(generic_output.data, generic_output.cols, generic_output.rows, generic_output.channels());
-	ct::Image cpu_out(cpu_output.data, cpu_output.cols, cpu_output.rows, cpu_output.channels());
 	// generic
+	Halide::Runtime::Buffer<uint8_t> ctin(input.data, input.cols, input.rows, input.channels());
+	Halide::Runtime::Buffer<uint8_t> generic_out(generic_output.data, generic_output.cols, generic_output.rows, generic_output.channels());
 	auto start = std::chrono::high_resolution_clock::now();
-	auto ret = ct::calc_back_projection_generic(&generic_out, ctin);
+	auto error = back_projection_basic(ctin, generic_out);
 	auto end = std::chrono::high_resolution_clock::now();
-	EXPECT_EQ(ct::Result::OK, ret);
-	auto s_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-	// cpu
-	start = std::chrono::high_resolution_clock::now();
-	ret = ct::calc_back_projection_cpu(&cpu_out, ctin);
-	end = std::chrono::high_resolution_clock::now();
-	EXPECT_EQ(ct::Result::OK, ret);
-	auto p_msec = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-	std::cout << "generic:" << s_msec << " msec" << std::endl;
-	std::cout << "cpu:" << p_msec << " msec" << std::endl;
+	EXPECT_EQ(0, error);
+	auto s_usec = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+	std::cout << "generic:" << s_usec << " usec" << std::endl;
 	cv::imwrite("lena_generic_out.png", generic_output);
-	cv::imwrite("lena_cpu_out.png", cpu_output);
 }
