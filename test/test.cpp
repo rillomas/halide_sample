90dbe7af89046bc941f758d5534969be852a814b
Masato Hori <rillomas@gmail.com>
2019-04-18 15:25:36 +0900

    added different versions of backends

--- test/test.cpp
+++ test/test.cpp
@@ -3,9 +3,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <chrono>
 #include "HalideBuffer.h"
-#include "back_projection_basic.h"
+#include "back_projection_generic.h"
+#include "back_projection_cpu.h"
 
-TEST(IOTest, CanReadAndWrite) {
+TEST(IOTest, ExecuteGeneral) {
 	const std::string in_path("../test/data/lena.png");
 	cv::Mat input = cv::imread(in_path);
 	ASSERT_FALSE(input.empty());
@@ -14,11 +15,28 @@ TEST(IOTest, CanReadAndWrite) {
 	// generic
 	Halide::Runtime::Buffer<uint8_t> ctin(input.data, input.cols, input.rows, input.channels());
 	Halide::Runtime::Buffer<uint8_t> generic_out(generic_output.data, generic_output.cols, generic_output.rows, generic_output.channels());
-	auto start = std::chrono::high_resolution_clock::now();
-	auto error = back_projection_basic(ctin, generic_out);
-	auto end = std::chrono::high_resolution_clock::now();
-	EXPECT_EQ(0, error);
-	auto s_usec = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-	std::cout << "generic:" << s_usec << " usec" << std::endl;
+	auto gen_start = std::chrono::high_resolution_clock::now();
+	auto gen_error = back_projection_generic(ctin, generic_out);
+	auto gen_end = std::chrono::high_resolution_clock::now();
+	EXPECT_EQ(0, gen_error);
+	auto gen_time = std::chrono::duration_cast<std::chrono::microseconds>(gen_end-gen_start).count();
+	std::cout << "generic:" << gen_time << " usec" << std::endl;
 	cv::imwrite("lena_generic_out.png", generic_output);
+}
+
+TEST(IOTest, ExecuteCPU) {
+	const std::string in_path("../test/data/lena.png");
+	cv::Mat input = cv::imread(in_path);
+	ASSERT_FALSE(input.empty());
+	cv::Mat cpu_output(input.size(), input.type());
+	// cpu
+	Halide::Runtime::Buffer<uint8_t> ctin(input.data, input.cols, input.rows, input.channels());
+	Halide::Runtime::Buffer<uint8_t> cpu_out(cpu_output.data, cpu_output.cols, cpu_output.rows, cpu_output.channels());
+	auto cpu_start = std::chrono::high_resolution_clock::now();
+	auto cpu_error = back_projection_cpu(ctin, cpu_out);
+	auto cpu_end = std::chrono::high_resolution_clock::now();
+	EXPECT_EQ(0, cpu_error);
+	auto cpu_time = std::chrono::duration_cast<std::chrono::microseconds>(cpu_end-cpu_start).count();
+	std::cout << "cpu:" << cpu_time << " usec" << std::endl;
+	cv::imwrite("lena_cpu_out.png", cpu_output);
 }
